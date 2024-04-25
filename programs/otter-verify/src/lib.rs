5087b17a32356303022ed65499ee73738acb3d73
swaroop-osec <swaroop@osec.io>
2024-04-25 09:29:30 +0530

    Added update ix

--- programs/otter-verify/src/lib.rs
+++ programs/otter-verify/src/lib.rs
@@ -1,4 +1,5 @@
 use anchor_lang::prelude::*;
+use anchor_lang::solana_program::system_program;
 
 declare_id!("72hPR1CB4gmUjUyBFBBdsvCcETAEn965kmZUm9sakrxN");
 
@@ -11,38 +12,79 @@ pub mod otter_verify {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>, params: InputParams) -> Result<()> {
+        // Print space of the account
+        msg!("Initialize otter-verify program");
+        msg!("Space: {}", ctx.accounts.build_params.to_account_info().data_len());
         let otter_verify = &mut ctx.accounts.build_params;
         otter_verify.command = params.command;
         otter_verify.bump = ctx.bumps.build_params;
         Ok(())
     }
+
+    pub fn update(ctx: Context<Update>, params: InputParams) -> Result<()> {
+        msg!("Update otter-verify program");
+        msg!("Space: {}", ctx.accounts.build_params.to_account_info().data_len());
+        let otter_verify = &mut ctx.accounts.build_params;
+        otter_verify.command = params.command;
+        Ok(())
+    }
+}
+
+
+fn calculate_space(input: &[String]) -> usize {
+    // 8 bytes for discriminator 
+    // 4 bytes for length of the vector
+    // 4 + len bytes for each string in the vector
+    // 1 byte for bump
+    8 + 4 + input.iter().map(|x| 4+x.len()).sum::<usize>() + 1
 }
 
 #[derive(Accounts)]
+#[instruction(instruction_data: InputParams)]
 pub struct Initialize<'info> {
-    // Space : 8 discriminator + 2 * 500 (command) + 1 bump
     #[account(
         init, 
         seeds = [PDA_SEED, authority.key().as_ref(), program_address.key().as_ref()],
         bump,
         payer = authority, 
-        space = 8 + 2 * BuildParams::INIT_SPACE, 
+        space =  calculate_space(&instruction_data.command) 
     )]
     pub build_params: Account<'info, BuildParams>,
     #[account(mut)]
     pub authority: Signer<'info>,
     /// CHECK:
     pub program_address: AccountInfo<'info>,
+    #[account(address = system_program::ID)]
+    pub system_program: Program<'info, System>,
+}
+
+
+
+
+#[derive(Accounts)]
+#[instruction(instruction_data: InputParams)]
+pub struct Update<'info> {
+    #[account(
+        mut,
+        realloc = calculate_space(&instruction_data.command),
+        realloc::zero = false, 
+        realloc::payer=authority
+    )]
+    pub build_params: Account<'info, BuildParams>,
+    #[account(mut)]
+    pub authority: Signer<'info>,
+    /// CHECK:
+    pub program_address: AccountInfo<'info>,
+    #[account(address = system_program::ID)]
     pub system_program: Program<'info, System>,
 }
 
 #[account]
-#[derive(InitSpace)]
 pub struct BuildParams {
-    #[max_len(10, 25)]
     pub command: Vec<String>,
     bump: u8,
 }
+
 
 #[derive(AnchorSerialize, AnchorDeserialize, Eq, PartialEq, Clone, Debug)]
 pub struct InputParams {
--- tests/otter-verify.ts
+++ tests/otter-verify.ts
@@ -1,44 +1,47 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { OtterVerify } from "../target/types/otter_verify";
+import { assert } from "console";
 
-describe("otter-verify", () => {
+describe("otter-verify", async () => {
   // Configure the client to use the local cluster.
   let provider = anchor.AnchorProvider.env();
   anchor.setProvider(provider);
 
   const program = anchor.workspace.OtterVerify as Program<OtterVerify>;
+  const user = anchor.web3.Keypair.generate();
+
+  let other_program_id = new anchor.web3.PublicKey(
+    "PhoeNiXZ8ByJGLkxNfZRnkUfjvmuYqLR89jjFHGqdXY"
+  );
+
+  const [otter_verify_pda, _] =
+    await anchor.web3.PublicKey.findProgramAddressSync(
+      [
+        anchor.utils.bytes.utf8.encode("otter_verify"),
+        user.publicKey.toBuffer(),
+        other_program_id.toBuffer(),
+      ],
+      program.programId
+    );
 
   it("Is initialized!", async () => {
-    // Add your test here.
-
-    const user = anchor.web3.Keypair.generate();
     // Airdrop some SOL to the user.
-    let airdrop_tx = await provider.connection.requestAirdrop(user.publicKey, 1000000000);
+    let airdrop_tx = await provider.connection.requestAirdrop(
+      user.publicKey,
+      1000000000
+    );
     await provider.connection.confirmTransaction(airdrop_tx, "confirmed");
-
-    let other_program_id = new anchor.web3.PublicKey("PhoeNiXZ8ByJGLkxNfZRnkUfjvmuYqLR89jjFHGqdXY");
-
-    const [otter_verify_pda, _] =
-      await anchor.web3.PublicKey.findProgramAddressSync(
-        [
-          anchor.utils.bytes.utf8.encode("otter_verify"),
-          user.publicKey.toBuffer(),
-          other_program_id.toBuffer(),  
-        ],
-        program.programId
-      );
 
     await program.methods
       .initialize({
         command: [
-          "https://github.com/Ellipsis-Labs/phoenix-v1",
           "--commit",
           "098551f",
         ],
       })
       .accounts({
-        buildParamsInit: otter_verify_pda,
+        buildParams: otter_verify_pda,
         authority: user.publicKey,
         programAddress: other_program_id,
         systemProgram: anchor.web3.SystemProgram.programId,
@@ -46,9 +49,31 @@ describe("otter-verify", () => {
       .signers([user])
       .rpc();
 
-        let params = await program.account.buildParams.fetch(otter_verify_pda);
-        console.log(params);
+    let params = await program.account.buildParams.fetch(otter_verify_pda);
+    assert(params.command[0] == "--commit");
+    assert(params.command.length == 2);
   });
 
-
+  it("Is Updated!", async () => {
+    await program.methods
+      .update({
+        command: [
+          "https://github.com/Ellipsis-Labs/phoenix-v1",
+          "--commit",
+          "098551f",
+        ],
+      })
+      .accounts({
+        buildParams: otter_verify_pda,
+        authority: user.publicKey,
+        programAddress: other_program_id,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      })
+      .signers([user])
+      .rpc();
+    let buildParams = await program.account.buildParams.fetch(otter_verify_pda);
+    assert(
+      buildParams.command[0] == "https://github.com/Ellipsis-Labs/phoenix-v1"
+    );
+  });
 });
